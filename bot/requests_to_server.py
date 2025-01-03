e002c04646d2a953b82b5c1b0c3f9cd130af9d75
Lev Kerskiy-Gosling <levker08@icloud.com>
2025-01-03 21:19:35 +0300

    доделал запросы

--- bot/requests_to_server.py
+++ bot/requests_to_server.py
@@ -30,16 +30,21 @@ async def get_char_by_char_id(char_id: int):
 
 async def create_char(char_data: dict):
     response = httpx.post(f"{os.getenv("QUESTHUB_URL")}/api/v1/characters/char-list",json=char_data)
-    return response.json()#ПОКА НЕ РАБОТАЕТ
+    return response.json()
 
-async def update_char(char_data: dict):
-    response = httpx.put(f"{os.getenv("QUESTHUB_URL")}/api/v1/characters/char-list",json=char_data)
-    return response.json()#ПОКА НЕ РАБОТАЕТ
+async def update_char(char_data: dict, char_id: int):
+    response = httpx.put(f"{os.getenv("QUESTHUB_URL")}/api/v1/characters/char-list",json=char_data,params={"character_id": char_id})
+    return response.json()
 
 async def delete_char(char_id: int):
     response = httpx.delete(f"{os.getenv("QUESTHUB_URL")}/api/v1/characters/char-list/{char_id}")
-    return response.json()#ПОКА НЕ РАБОТАЕТ
+    return response.json()
 
 async def get_char_by_user_id(user_id: int):
     response = httpx.get(f"{os.getenv("QUESTHUB_URL")}/api/v1/characters/char-list/{user_id}/")
-    return response.json()#ПОКА НЕ РАБОТАЕТ
+    return response.json()
+
+#запрос к mvp от rnd
+async def auto_create_char(auto_char_data: dict):
+    response = httpx.post(f"{os.getenv("RND_URL")}/create-character-list",json=auto_char_data)
+    return response.json()
