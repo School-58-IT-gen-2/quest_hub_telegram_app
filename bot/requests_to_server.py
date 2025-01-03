7bffe6ec832ea1469a2167319c869bd19e9bfba1
Lev Kerskiy-Gosling <levker08@icloud.com>
2025-01-03 18:31:05 +0300

    просто добавил неработающий код ,_,

--- bot/requests_to_server.py
+++ bot/requests_to_server.py
@@ -23,4 +23,23 @@ async def delete_user(tg_id: int):
     response = httpx.delete(f"{os.getenv("QUESTHUB_URL")}/api/v1/profiles/user",params={"tg_id": tg_id})
     return response.json()
 
+#запросы для персонажей
+async def get_char_by_char_id(char_id: int):
+    response = httpx.get(f"{os.getenv("QUESTHUB_URL")}/api/v1/characters/char-list/{char_id}")
+    return response.json() #ПОКА НЕ РАБОТАЕТ
 
+async def create_char(char_data: dict):
+    response = httpx.post(f"{os.getenv("QUESTHUB_URL")}/api/v1/characters/char-list",json=char_data)
+    return response.json()
+
+async def update_char(char_data: dict):
+    response = httpx.put(f"{os.getenv("QUESTHUB_URL")}/api/v1/characters/char-list",json=char_data)
+    return response.json()
+
+async def delete_char(char_id: int):
+    response = httpx.delete(f"{os.getenv("QUESTHUB_URL")}/api/v1/characters/char-list/{char_id}")
+    return response.json()
+
+async def get_char_by_user_id(user_id: int):
+    response = httpx.get(f"{os.getenv("QUESTHUB_URL")}/api/v1/characters/char-list/{user_id}/")
+    return response.json()
